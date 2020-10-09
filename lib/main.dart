aa24f920ba09e6aceb6939eecaa77ccb5ee9d958
v.reshetnikov <skemobob@gmail.com>
2020-10-09 18:58:12 +0300

    shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,90 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  runApp(MyApp());
+  // debugPaintSizeEnabled = true;
+  runApp(KanbanApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class KanbanApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Govno',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'My Plans'),
+          _buildButtonColumn(color, Icons.near_me, 'Visited Places'),
+          _buildButtonColumn(color, Icons.share, 'Share experience'),
+        ],
       ),
-      home: MyHomePage(title: 'Zalupa Customs'),
     );
-  }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Position position;
-  void _incrementCounter() async {
-    position = await getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-      position = position;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+    return MaterialApp(
+      title: 'My Journeys',
+      home: Scaffold(
+        appBar: AppBar(
+            title: new Center(
+                child: new Text(
+          'My Journeys',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Raleway',
+            fontSize: 22.0,
+          ),
+        ))),
+        body: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-                (position != null && position.latitude != null)
-                    ? position.toString()
-                    : "",
-                style: Theme.of(context).textTheme.headline6,
-                textAlign: TextAlign.center)
+            Stack(children: [
+              Container(
+                  alignment: Alignment.center,
+                  child: Image.asset(
+                    'images/koster.jpg',
+                    // width: 600,
+                    // height: 240,
+                    fit: BoxFit.fill,
+                  )),
+              // titleSection,
+              // buttonSection,
+              // textSection,
+              _buildButtonColumn(color, Icons.ac_unit, "Let's travel")
+            ]),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.laptop_windows),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Row _buildButtonColumn(Color color, IconData icon, String label) {
+    return Row(
+      // mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Icon(icon, color: color),
+        // Container(
+        // alignment: Alignment.center,
+        // child:
+        FlatButton(
+          shape: new RoundedRectangleBorder(
+            borderRadius: new BorderRadius.circular(30.0),
+          ),
+          onPressed: () {},
+          color: Colors.blue[300],
+          child: Text(
+            label,
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Raleway',
+              fontSize: 22.0,
+            ),
+          ),
+        ),
+        // ),
+      ],
     );
   }
 }
